2eea764a4528a734c5b67335547f2b1cbc66ad62
HONG NHUNG <nhungpthph41518@fpt.edu.vn>
2023-11-16 10:59:53 +0700

    nhung

---
+++ app/src/main/java/da1/qlhd/nhom3/DoiMatKhau.java
@@ -0,0 +1,14 @@
+package da1.qlhd.nhom3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoiMatKhau extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doi_mat_khau);
+    }
+}
